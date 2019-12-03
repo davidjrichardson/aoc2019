6d8d99b3a65734bd62b3b100708ac70842d1477e
David Richardson <david@tankski.co.uk>
2019-12-03 10:13:06 +0000

    Finish question 2a
    OH MY GOD IT WORKED

--- day_2/Answers.hs
+++ day_2/Answers.hs
@@ -3,13 +3,19 @@ import Input
 import qualified Data.Map as Map
 
 -- Question 1
+addcode :: Int -> Int -> Int -> (Map.Map Int Int) -> (Map.Map Int Int)
+addcode verb noun place map = Map.insert place (map Map.! noun + map Map.! verb) map
+
+multcode :: Int -> Int -> Int -> (Map.Map Int Int) -> (Map.Map Int Int)
+multcode verb noun place map = Map.insert place (map Map.! noun * map Map.! verb) map
+
 intcode :: [Int] -> (Map.Map Int Int) -> (Map.Map Int Int)
-intcode codes state
-    | oper -- Pattern match over oper; what do
-    | otherwise     = state
-    where
-        oper = take 4 codes
+intcode (1  : verb : noun : place : rest) state = intcode rest (addcode verb noun place state)
+intcode (2  : verb : noun : place : rest) state = intcode rest (multcode verb noun place state)
+intcode codes state = state
 
-
+-- Create a Map from the codes so I can refer by index
 state :: Map.Map Int Int
-state = Map.fromList []
+state = Map.fromList (zip [0..1000] values)
+
+result = (intcode values state) Map.! 0
