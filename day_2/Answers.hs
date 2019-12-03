e9b720eaf9e6a63c54fd0b9495f1d2bd95f6706f
David Richardson <david@tankski.co.uk>
2019-12-03 14:08:32 +0000

    Extract opcode function to be a param

--- day_2/Answers.hs
+++ day_2/Answers.hs
@@ -5,21 +5,18 @@ import qualified Data.List as List
 import qualified Data.Maybe as Maybe
 
 -- Functions for processing Intcode
-addcode :: Int -> Int -> Int -> (Map.Map Int Int) -> (Map.Map Int Int)
-addcode verb noun place map = Map.insert place (map Map.! noun + map Map.! verb) map
-
-multcode :: Int -> Int -> Int -> (Map.Map Int Int) -> (Map.Map Int Int)
-multcode verb noun place map = Map.insert place (map Map.! noun * map Map.! verb) map
+opcode :: Int -> Int -> Int -> (Map.Map Int Int) -> (Int -> Int -> Int) -> (Map.Map Int Int)
+opcode verb noun place map f = Map.insert place (f (map Map.! noun) (map Map.! verb)) map
 
 intcode :: [Int] -> (Map.Map Int Int) -> (Map.Map Int Int)
-intcode (1  : verb : noun : place : rest) state = intcode rest (addcode verb noun place state)
-intcode (2  : verb : noun : place : rest) state = intcode rest (multcode verb noun place state)
+intcode (1  : verb : noun : place : rest) state = intcode rest (opcode verb noun place state (+))
+intcode (2  : verb : noun : place : rest) state = intcode rest (opcode verb noun place state (*))
 intcode codes state = state
 
 -- Question 1
 -- Create a Map from the codes so I can refer by index
 state :: Map.Map Int Int
-state = Map.fromList (zip [0..1000] values)
+state = Map.fromList (zip [0..] values)
 
 result = (intcode values state) Map.! 0
 
@@ -33,7 +30,7 @@ changeParam :: (Int, Int) -> [Int]
 changeParam pair = 1:(fst pair):(snd pair):(drop 3 values)
 
 changeState :: (Int, Int) -> (Map.Map Int Int)
-changeState pair = Map.insert 2 (snd pair) (Map.insert 1 (fst pair) (Map.fromList (zip [0..1000] values)))
+changeState pair = Map.insert 2 (snd pair) (Map.insert 1 (fst pair) (Map.fromList (zip [0..] values)))
 
 codeSpace = map changeParam paramSpace
 stateSpace = map changeState paramSpace
@@ -42,4 +39,4 @@ programs = zip codeSpace stateSpace
 
 -- Map function across (Int, Int)
 testProgram program = ((intcode (fst program) (snd program)) Map.! 0)
-results_2 = paramSpace !! Maybe.fromMaybe 0 (List.elemIndex values2 (map testProgram programs))
+result_2 = paramSpace !! Maybe.fromMaybe 0 (List.elemIndex values2 (map testProgram programs))
