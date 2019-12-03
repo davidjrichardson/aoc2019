10c5ef8215109739137cb1696bc9c827c050bf28
David Richardson <david@tankski.co.uk>
2019-12-03 12:45:54 +0000

    Finish day 2
    ghfsubfdahlifasiofjkadofknzxcvb bywgr isa :shipit:

--- day_2/Answers.hs
+++ day_2/Answers.hs
@@ -1,6 +1,8 @@
 -- Day one of the Advent of Code
 import Input
 import qualified Data.Map as Map
+import qualified Data.List as List
+import qualified Data.Maybe as Maybe
 
 -- Functions for processing Intcode
 addcode :: Int -> Int -> Int -> (Map.Map Int Int) -> (Map.Map Int Int)
@@ -22,8 +24,10 @@ state = Map.fromList (zip [0..1000] values)
 result = (intcode values state) Map.! 0
 
 -- Question 2
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
 paramSpace :: [(Int, Int)]
-paramSpace = zip [12..1000] [2..1000]
+paramSpace = cartProd [0..99] [0..99]
 
 changeParam :: (Int, Int) -> [Int]
 changeParam pair = 1:(fst pair):(snd pair):(drop 3 values)
@@ -38,5 +42,4 @@ programs = zip codeSpace stateSpace
 
 -- Map function across (Int, Int)
 testProgram program = ((intcode (fst program) (snd program)) Map.! 0)
-filterVal x = x == values2
-results_2 = filter filterVal (map testProgram programs)
+results_2 = paramSpace !! Maybe.fromMaybe 0 (List.elemIndex values2 (map testProgram programs))
